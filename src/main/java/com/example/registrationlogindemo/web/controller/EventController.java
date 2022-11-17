a9c42001a0c6355fba78a79697eab7939f3a1e3d
Scheibli Bence <scheibli.bence@gmail.com>
2022-11-17 19:45:53 +0100

    event_controller_updated

--- src/main/java/com/example/registrationlogindemo/web/controller/EventController.java
+++ src/main/java/com/example/registrationlogindemo/web/controller/EventController.java
@@ -1,16 +1,15 @@
 package com.example.registrationlogindemo.web.controller;
 
+import com.example.registrationlogindemo.persistence.entity.EventEntity;
 import com.example.registrationlogindemo.service.EventService;
 import com.example.registrationlogindemo.web.model.CreateEventRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -19,6 +18,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class EventController {
     private EventService eventService;
 
+    @GetMapping("/get_event")
+    public String createEventView(Model model) {
+        model.addAttribute("event", new EventEntity());
+        return "create_event";
+    }
+
+    @PostMapping(path = "/create_event")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addEvent(@ModelAttribute("event") CreateEventRequest createEventRequest, Model model) throws Exception {
+        model.addAttribute("event", createEventRequest);
+        eventService.addEvent(createEventRequest);
+        return "event_created";
+    }
     //Endpoints for Postman
     //------------------------------------------------------------------------------------------------------------------
 
