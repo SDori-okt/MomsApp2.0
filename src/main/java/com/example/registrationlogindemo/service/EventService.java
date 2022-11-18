acae22d5e3f7e1d6b6385056ce7bab3aed694d02
Scheibli Bence <scheibli.bence@gmail.com>
2022-11-18 12:10:24 +0100

    event_handling

--- src/main/java/com/example/registrationlogindemo/service/EventService.java
+++ src/main/java/com/example/registrationlogindemo/service/EventService.java
@@ -7,6 +7,8 @@ import com.example.registrationlogindemo.web.model.CreateEventRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class EventService {
@@ -14,6 +16,10 @@ public class EventService {
     private EventRepository eventRepository;
 
     private EventMapper eventMapper;
+
+    public List<EventEntity> findAll(){
+        return eventRepository.findAll();
+    }
 
     public EventEntity addEvent(CreateEventRequest createEventRequest) {
         EventEntity event = eventMapper.map(createEventRequest);
--- src/main/java/com/example/registrationlogindemo/web/controller/EventController.java
+++ src/main/java/com/example/registrationlogindemo/web/controller/EventController.java
@@ -1,8 +1,10 @@
 package com.example.registrationlogindemo.web.controller;
 
 import com.example.registrationlogindemo.persistence.entity.EventEntity;
+import com.example.registrationlogindemo.persistence.entity.UserEntity;
 import com.example.registrationlogindemo.service.EventService;
 import com.example.registrationlogindemo.web.model.CreateEventRequest;
+import com.example.registrationlogindemo.web.model.CreateUserRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @AllArgsConstructor
 @Validated
@@ -18,8 +22,15 @@ import org.springframework.web.bind.annotation.*;
 public class EventController {
     private EventService eventService;
 
-    @GetMapping("/get_event")
-    public String createEventView(Model model) {
+    @RequestMapping("/events")
+    public String getEvents(Model model) {
+        List<EventEntity> events = eventService.findAll();
+        model.addAttribute("events", events);
+        return "events";
+    }
+
+    @GetMapping("/create_event")
+    public String createEventForm(Model model) throws Exception {
         model.addAttribute("event", new EventEntity());
         return "create_event";
     }
@@ -28,7 +39,11 @@ public class EventController {
     @ResponseStatus(HttpStatus.CREATED)
     public String addEvent(@ModelAttribute("event") CreateEventRequest createEventRequest, Model model) throws Exception {
         model.addAttribute("event", createEventRequest);
-        eventService.addEvent(createEventRequest);
+        try {
+            eventService.addEvent(createEventRequest);
+        } catch (Exception e){
+            return "create_event_error";
+        }
         return "event_created";
     }
     //Endpoints for Postman
