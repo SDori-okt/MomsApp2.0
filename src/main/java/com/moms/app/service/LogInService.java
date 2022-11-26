7e24d6e0b89c6f5a469831e05d06c457447cdd99
Scheibli Bence <scheibli.bence@gmail.com>
2022-11-26 12:31:37 +0100

    loginService update

--- src/main/java/com/moms/app/service/LogInService.java
+++ src/main/java/com/moms/app/service/LogInService.java
@@ -1,9 +1,15 @@
 package com.moms.app.service;
 
+import com.moms.app.configuration.MyAuth;
 import com.moms.app.persistence.entity.UserEntity;
 import com.moms.app.persistence.repository.UserRepository;
+import com.moms.app.security.UserDetailsPrincipal;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,11 +18,11 @@ import java.util.Optional;
 
 @Service
 @AllArgsConstructor
-public class LogInService {
+public class LogInService implements UserDetailsService {
 
     private UserRepository userRepository;
 
-    public void logIn(UserEntity user) {
+    public UserDetails logIn(UserEntity user) {
         Optional<UserEntity> maybeUser = userRepository.findByUsername(user.getUsername());
 
         if (maybeUser.isEmpty()) {
@@ -24,11 +30,16 @@ public class LogInService {
         } else {
             boolean matches = new BCryptPasswordEncoder().matches(user.getPassword(), maybeUser.get().getPassword());
             if (matches) {
-                // todo load user
+                SecurityContextHolder.getContext().setAuthentication(new MyAuth(user.getUsername()));
+                return loadUserByUsername(user.getUsername());
             } else {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
             }
         }
     }
-
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> byUsername = userRepository.findByUsername(username);
+        return new UserDetailsPrincipal(byUsername.get());
+    }
 }
